b2a1725024f883eb993b6093c67f15839c86ae78
Rene Alejandro Mendoza Muñoz <rene.mendozam@bci.cl>
2025-09-11 00:59:55 -0300

    mejora main2

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";           // 👈 importar el DEFAULT, no { AppWrapper }
 import "./styles/custom.css";
-import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import App from './App.jsx'
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-)
